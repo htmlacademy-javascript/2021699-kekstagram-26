6d07bdbb20abefdfd55fba59998946076e09aea7
Elvina shamsutdinova <kuku1802shka@mail.ru>
2022-06-10 22:40:35 +0300

    Работа над ошибками

--- js/main.js
+++ js/main.js
@@ -1,14 +1,15 @@
-function randomInteger(min,max){
-  if(min>=0 && max>=0 && min<max){
-    return Math.floor(Math.random() * (max - min + 1) ) + min;
+const randomInteger = (min, max) => {
+  if (min < 0 && max < 0 && min > max){
+    throw new RangeError('ошибка');
   }
-}
-randomInteger();
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
+};
+randomInteger(1, 10);
 
-function lengthCheck (line,maxLength) {
-  if(maxLength>=140 && line>=maxLength){
-    return(lengthCheck.slice(0,maxLength-1));
+const lengthCheck = (line, maxLength) => {
+  if (maxLength <= 140 && line.length >= maxLength){
+    return false;
   }
-  return(false);
-}
-lengthCheck();
+  return true;
+};
+lengthCheck('2570920', 10);
