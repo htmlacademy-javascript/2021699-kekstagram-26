4558df772ae6823e660e83baacdfad4cdb1c8f7a
Elvina shamsutdinova <kuku1802shka@mail.ru>
2022-06-10 22:59:10 +0300

    Коррекция работы над ошибкой

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
-const randomInteger = (min, max) => {
+const getrandomInteger = (min, max) => {
   if (min < 0 && max < 0 && min > max){
-    throw new RangeError('ошибка');
+    throw new RangeError('Переданы некорректные параметры');
   }
   return Math.floor(Math.random() * (max - min + 1) ) + min;
 };
-randomInteger(1, 10);
+getrandomInteger(1, 10);
 
-const lengthCheck = (line, maxLength) => {
-  if (maxLength <= 140 && line.length >= maxLength){
+const checkLength = (line, maxLength) => {
+  if (line.length >= maxLength){
     return false;
   }
   return true;
 };
-lengthCheck('2570920', 10);
+checkLength('2570920', 140);
