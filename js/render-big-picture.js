0dae2b3c53a2b212b94d4bae4714c8beb6eca37f
Elvina shamsutdinova <kuku1802shka@mail.ru>
2022-07-06 00:10:09 +0300

    открытие модального окна

--- js/render-big-picture.js
+++ js/render-big-picture.js
@@ -1,20 +1,16 @@
-// import {renderPictures} from './render-picture.js';
+import {body} from './render-picture.js';
 
 const popup = document.querySelector('.big-picture');
 
 popup.addEventListener('click', () => {
   popup.classList.add('hidden');
+  body.classList.remove('modal-open');
+
 });
 
 document.addEventListener('keydown', (evt) => {
   if (evt.keyCode === 27) {
     popup.classList.add('hidden');
+    body.classList.remove('modal-open');
   }
 });
-
-// const  bigPicture = function() {
-//   const pictures = document.querySelectorAll('.pictures');
-//   console.log(pictures);
-// };
-
-// bigPicture();
--- js/render-picture.js
+++ js/render-picture.js
@@ -4,7 +4,6 @@ const pictureTemplate = document.querySelector('#picture').content.querySelector
 const commentCount = document.querySelector('.social__comment-count');
 const commentsLoader = document.querySelector('.social__comments-loader');
 const body = document.querySelector('body');
-const commentList = document.querySelector('.social__comments');
 
 const renderPictures = (pictures) => {
   const pictureListFragment = document.createDocumentFragment();
@@ -13,54 +12,66 @@ const renderPictures = (pictures) => {
     const pictureElement = pictureTemplate.cloneNode(true);
     pictureElement.querySelector('.picture__img').src = photo.url;
     pictureElement.querySelector('.picture__comments').textContent = photo.comments.length;
-    pictureElement.querySelector('.picture__likes').textContent = photo.likes;
+    pictureElement.querySelector('.picture__likes').textContent = photo.likes.length;
     pictureListFragment.appendChild(pictureElement);
 
-    pictureElement.addEventListener('click', (photo) => {
+    pictureElement.addEventListener('click', () => {
       const popup = document.querySelector('.big-picture');
+
+      // Открываем модалку
       popup.classList.remove('hidden');
       body.classList.add('modal-open');
+
       commentCount.classList.add('hidden');
       commentsLoader.classList.add('hidden');
 
-      popup.querySelector('.big-picture__img').src = photo.url;
-      popup.querySelector('.comments-count').textContent = photo.comments.length;
-      popup.querySelector('.likes-count').textContent = photo.likes;
+      // Картинка модалки
+      popup.querySelector('.big-picture__img img').src = photo.url;
       popup.querySelector('.social__caption').textContent = photo.description;
+      popup.querySelector('.likes-count').textContent = photo.likes;
+      popup.querySelector('.comments-count').textContent = photo.comments.length;
 
-      const makeComment = function (tagName, className, text) {
-        const commentElement = document.createElement(tagName);
-        commentElement.classList.add(className);
-        if (text) {
-          commentElement.textContent = text;
-        }
-        return commentElement;
-      };
-
-
-      const creatComment = function (socialComments) {
-        for( let i = 0; i < socialComments.length; i++){
-          const li = makeComment('li','social__comment');
-          commentList.appendChild(li);
-
-          const img = makeComment('img','social__comment');
-          img.src = socialComments[i].src;
-          img.alt = socialComments[i].alt;
-          img.width='35';
-          img.height='35';
-          li.appendChild(img);
-
-          const p = document.createElement('p');
-          p.classList.add('social__text');
-          p.textContent = 'текст комментария';
-          li.appendChild(p);
-        }
-      };
-      creatComment();
+      // Удаляем комментарии,которые были
+      popup.querySelector('.social__comments').innerHTML = '';
     });
   });
   pictureListElement.appendChild(pictureListFragment);
 };
 
 
-export {renderPictures};
+// функция создания коммента
+function creatComments(comment){
+
+  // создаем лишку
+  const oneComment = document.createElement('li');
+  oneComment.classList.add('social__comment');
+  // commentList.appendChild(oneComment);
+
+  // картинка
+  const oneImg = document.createElement('img');
+  oneImg.classList.add('social__picture');
+  oneImg.src=comment.avatar;
+  oneImg.alt=comment.name;
+  oneComment.appendChild(oneImg);
+
+
+  // добавляем абзац
+  const commentText = document.createElement('p');
+  commentText.classList.add('social__text');
+  commentText.textContent = comment.message;
+  oneComment.appendChild(commentText);
+
+  return oneComment;
+}
+
+// перебор коллекции элементов
+const commentList = document.querySelector('.social__comments');
+for (let i = 0; i < commentList.length; i++) {
+  const cardItem = commentList[i];
+  commentList.appendChild(cardItem);
+}
+
+
+creatComments(commentList);
+
+export {renderPictures, body};
