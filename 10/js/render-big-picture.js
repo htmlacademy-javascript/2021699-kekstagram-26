07b5d0aaf02cb5e453cd4b89b6d074e21fbe1cd7
Keks <keks@htmlacademy.ru>
2022-07-22 02:24:13 +0300

    :heavy_check_mark: Сборка #10

---
+++ 10/js/render-big-picture.js
@@ -0,0 +1,105 @@
+const popup = document.querySelector('.big-picture');
+const commentCount = document.querySelector('.social__comment-count');
+const commentCountSlice = commentCount.querySelector('.comments-count__slice');
+const commentsContainer = popup.querySelector('.social__comments');
+const сloseButton = popup.querySelector('#picture-cancel');
+const uploadButton = popup.querySelector('.comments-loader');
+let displayedComments = 5;
+
+const closeModal = () => {
+  popup.classList.add('hidden');
+  document.body.classList.remove('modal-open');
+  document.removeEventListener('keydown', onDocumentKeyDown);
+};
+
+const openModal = () => {
+  popup.classList.remove('hidden');
+  document.body.classList.add('modal-open');
+  document.addEventListener('keydown', onDocumentKeyDown);
+  сloseButton.addEventListener('click', () => closeModal());
+};
+
+function onDocumentKeyDown (evt) {
+  if (evt.key === 'Escape') {
+    closeModal();
+  }
+}
+
+const createComment = (comment) =>{
+  const oneComment = document.createElement('li');
+  oneComment.classList.add('social__comment');
+  // картинка
+  const oneImg = document.createElement('img');
+  oneImg.classList.add('social__picture');
+  oneImg.src = comment.avatar;
+  oneImg.alt = comment.name;
+  oneComment.appendChild(oneImg);
+
+  // добавляем абзац
+  const commentText = document.createElement('p');
+  commentText.classList.add('social__text');
+  commentText.textContent = comment.message;
+  oneComment.appendChild(commentText);
+
+  return oneComment;
+};
+
+const createComments = (comments) => {
+  const fragment = document.createDocumentFragment();
+
+  comments.forEach((comment) => {
+    const renderedComment = createComment(comment);
+    fragment.appendChild(renderedComment);
+  });
+
+  return fragment;
+};
+
+const renderBigPicture = (photo) => {
+
+  openModal();
+
+
+  // Картинка модалки
+  popup.querySelector('.big-picture__img img').src = photo.url;
+  popup.querySelector('.social__caption').textContent = photo.description;
+  popup.querySelector('.likes-count').textContent = photo.likes;
+  popup.querySelector('.comments-count').textContent = photo.comments.length;
+
+
+  //Комменты
+  // Удаляем комментарии,которые были
+  popup.querySelector('.social__comments').innerHTML = '';
+  //массив готовых комментов
+  // let commentsPhoto = createComments(photo.comments);
+
+
+  const totalcomments = photo.comments.length;
+  commentCountSlice.innerHTML = displayedComments;
+  const op = () => {
+    if(totalcomments === displayedComments){
+      let photoComments = displaysCertainNumberComments();
+      let partComments = createComments(photoComments);
+      commentsContainer.appendChild(partComments);
+    }
+
+    uploadButton.addEventListener('click', () => {
+      displayedComments += 5;
+      if(displayedComments !== totalcomments){
+        let photoComments = displaysCertainNumberComments();
+        let partComments = createComments(photoComments);
+        commentsContainer.appendChild(partComments);
+      }else {uploadButton.classList.add('hidden');}
+    });
+  };
+
+  function displaysCertainNumberComments (){
+    return photo.comments.slice(0, displayedComments);
+  }
+  op();
+  let photoComments = displaysCertainNumberComments();
+  let partComments = createComments(photoComments);
+  commentsContainer.appendChild(partComments);
+};
+
+export {renderBigPicture};
---
+++ 10/js/render-picture.js
@@ -0,0 +1,24 @@
+import { renderBigPicture } from './render-big-picture.js';
+
+const pictureListElement = document.querySelector('.pictures');
+const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+
+const renderPictures = (pictures) => {
+  const pictureListFragment = document.createDocumentFragment();
+
+  pictures.forEach((photo) => {
+    const pictureElement = pictureTemplate.cloneNode(true);
+
+    pictureElement.querySelector('.picture__img').src = photo.url;
+    pictureElement.querySelector('.picture__comments').textContent = photo.comments.length;
+    pictureElement.querySelector('.picture__likes').textContent = photo.likes.length;
+    pictureListFragment.appendChild(pictureElement);
+
+    pictureElement.addEventListener('click', () => renderBigPicture(photo));
+  });
+
+  pictureListElement.appendChild(pictureListFragment);
+};
+
+
+export {renderPictures};
